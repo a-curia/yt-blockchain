7cce6aed4f0453db6cbaf57f69b96c68f38ba6bd
Gheorghe-Adrian Curia <gheorgheadrian.curia@ihsmarkit.com>
2019-06-12 10:23:41 +0300

    Signing transactions

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
-const {Blockchain, Transaction} = require('./blockchain.js');
+const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
+// Your private key goes here
+const myKey = ec.keyFromPrivate('7c4c45907dec40c91bab3480c39032e90049f1a44f3e18c3e07c23e3273995cf');
 
-const EC = require('elliptic').ec; // generate and check private and public keys
-const ec = new EC('secp256k1'); // algorithm
-
-const myKey = ec.keyFromPrivate('e93bb9602e3f055c7aad30b321116baae7dcc3948a3755337c5a3d1bb87944d3');
+// From that we can calculate your public key (which doubles as your wallet address)
 const myWalletAddress = myKey.getPublic('hex');
 
+// Create new instance of Blockchain class
+const savjeeCoin = new Blockchain();
 
-let testCoin = new Blockchain();
-
-const tx1 = new Transaction(myWalletAddress, 'pub-key-of-someone-else', 11);
+// Create a transaction & sign it with your key
+const tx1 = new Transaction(myWalletAddress, 'address2', 100);
 tx1.signTransaction(myKey);
-testCoin.addTransaction(tx1);
+savjeeCoin.addTransaction(tx1);
 
-console.log('\n Staring the miner.');
-testCoin.minePendingTransaction(myWalletAddress); // instead of sending them to another wallet send them to my wallet so we can accest it with public key
-console.log('\n Balance for address is ', testCoin.getBalanceOfAddress(myWalletAddress)); // same in here
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx2 = new Transaction(myWalletAddress, 'address1', 50);
+tx2.signTransaction(myKey);
+savjeeCoin.addTransaction(tx2);
+
+// Mine block
+savjeeCoin.minePendingTransactions(myWalletAddress);
+
+console.log();
+console.log(`Balance of xavier is ${savjeeCoin.getBalanceOfAddress(myWalletAddress)}`);
+
+// Uncomment this line if you want to test tampering with the chain
+// savjeeCoin.chain[1].transactions[0].amount = 10;
+
+// Check if the chain is valid
+console.log();
+console.log('Blockchain valid?', savjeeCoin.isChainValid() ? 'Yes' : 'No');
